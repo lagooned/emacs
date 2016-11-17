4a19eed8564f53cec7059c3fbcf60df371d0244c
Jared Engler <jmengl2@ilstu.edu>
2016-11-17 16:14:53 -0600

    refactored init lisp

--- init.el
+++ init.el
@@ -11,34 +11,21 @@
 		("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 
-;; install use package
-(if (not (package-installed-p 'use-package))
-	(progn
-	  (package-refresh-contents)
-	  (package-install 'use-package)))
-(setq use-package-always-ensure t)
-(require 'use-package)
-
 ;; add load path
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-;; config packages
-(load "config")
-
 ;; setup environment 
 (load "global")
+(load "config")
 
 ;; load custom settings
 (load "custom")
-
-;; custom 
-(custom-set-variables)
-(custom-set-faces)
-
-;; themes
-(load "themes")
 
 ;; motd
 (message "#################")
 (message "# CONFIG LOADED #")
 (message "#################")
+
+;; customs
+(custom-set-variables)
+(custom-set-faces)
