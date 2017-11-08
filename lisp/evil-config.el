f4b87107d48656fd341f2eefe2c875fff6e4e45c
jared engler <jared.lite@gmail.com>
2017-11-08 12:05:07 -0600

    removed annoying evil minibuffer messages

--- lisp/evil-config.el
+++ lisp/evil-config.el
@@ -85,5 +85,15 @@
        "r" 'revert-buffer
        "." 'dired-omit-mode)))
 
+;; minibuffer
+(setq evil-insert-state-message nil
+      evil-visual-state-message nil
+      evil-replace-state-message nil
+      evil-normal-state-message nil
+      evil-emacs-state-message nil
+      evil-motion-state-message nil
+      evil-insert-state-message nil
+      evil-operator-state-message nil)
+
 (provide 'evil-config)
 ;;; evil-config.el ends here
