44aa2b30d5b78c51ddc9ddbc417b2f89d537b7be
jared engler <jared.lite@gmail.com>
2017-11-09 03:15:20 -0600

    added elpa

---
+++ elpa/evil-vimish-fold-20171030.451/evil-vimish-fold-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-vimish-fold" "20171030.451" "Integrate vimish-fold with evil" '((emacs "24.4") (evil "1.0.0") (vimish-fold "0.2.0")) :commit "4db872d12274fdddf7c6e9d01cf68cbad9cfcf15" :url "https://github.com/alexmurray/evil-vimish-fold")
---
+++ elpa/ivy-hydra-20170703.2350/ivy-hydra-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-hydra" "20170703.2350" "Additional key bindings for Ivy" '((emacs "24.1") (ivy "0.9.0") (hydra "0.13.4")) :commit "5009b8149a86949f9e6d7546479e998be2e14337" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching" "bindings"))
