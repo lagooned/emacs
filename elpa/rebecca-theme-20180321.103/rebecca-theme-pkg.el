c0b2bbc53fe708c78b20f9cd0cdfe8c18682ba31
jared engler <jared.lite@gmail.com>
2018-03-23 00:12:56 -0500

    add rebecca theme

---
+++ elpa/rebecca-theme-20180321.103/rebecca-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rebecca-theme" "20180321.103" "Rebecca Purple Theme" '((emacs "24")) :commit "aae6dd4a27f9f1b62ce0b239fce104168c06dbc7" :url "https://github.com/vic/rebecca-theme" :keywords '("theme" "dark"))
