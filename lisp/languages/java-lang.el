250f1a32a8c8a56dae55040facb4701372a697a1
jared engler <jared.lite@gmail.com>
2019-01-15 23:03:46 -0600

    fix keywords in java-lang.el

--- lisp/languages/java-lang.el
+++ lisp/languages/java-lang.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2018  Jared M. Engler
 
 ;; Author: Jared M. Engler <jared.lite@gmail.com>
-;; Keywords: gmacs, config, languages, jav
+;; Keywords: gmacs, config, languages, java
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
