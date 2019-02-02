2a83b9e343b7c4bb157d6637e88d0e4768917a36
jared engler <jared.lite@gmail.com>
2019-02-02 13:15:29 -0600

    upgrade rainbow-delimiters

--- elpa/rainbow-delimiters-20170929.1132/rainbow-delimiters-autoloads.el
+++ elpa/rainbow-delimiters-20170929.1132/rainbow-delimiters-autoloads.el
@@ -1,14 +1,22 @@
 ;;; rainbow-delimiters-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "rainbow-delimiters" "rainbow-delimiters.el"
-;;;;;;  (23044 3567 0 0))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from rainbow-delimiters.el
 
 (autoload 'rainbow-delimiters-mode "rainbow-delimiters" "\
 Highlight nested parentheses, brackets, and braces according to their depth.
+
+If called interactively, enable Rainbow-Delimiters mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
 
 \(fn &optional ARG)" t nil)
 
@@ -22,11 +30,14 @@ Disable `rainbow-delimiters-mode'.
 
 \(fn)" nil nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rainbow-delimiters" '("rainbow-delimiters-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; rainbow-delimiters-autoloads.el ends here
