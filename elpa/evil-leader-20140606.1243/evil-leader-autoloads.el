91efc1809894dc3c4a1889b44fd1dd72e871f97d
jared engler <jared.lite@gmail.com>
2019-02-23 09:07:41 -0600

    update evil-leader

--- elpa/evil-leader-20140606.1243/evil-leader-autoloads.el
+++ elpa/evil-leader-20140606.1243/evil-leader-autoloads.el
@@ -1,19 +1,31 @@
 ;;; evil-leader-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "evil-leader" "evil-leader.el" (23044 3552
-;;;;;;  0 0))
+;;;### (autoloads nil "evil-leader" "evil-leader.el" (0 0 0 0))
 ;;; Generated autoloads from evil-leader.el
 
 (autoload 'global-evil-leader-mode "evil-leader" "\
 Global minor mode for <leader> support.
 
+If called interactively, enable Global Evil-Leader mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
 
 (autoload 'evil-leader-mode "evil-leader" "\
 Minor mode to enable <leader> support.
+
+If called interactively, enable Evil-Leader mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
 
 \(fn &optional ARG)" t nil)
 
@@ -32,11 +44,14 @@ See `evil-leader/set-key'.
 
 \(fn MODE KEY DEF &rest BINDINGS)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-leader" '("evil-leader")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; evil-leader-autoloads.el ends here
