bd43a70666db33909186284a7934413721b6fc5c
jared engler <jared.lite@gmail.com>
2019-08-31 22:40:50 -0500

    formatting

--- lisp/languages/ops-lang.el
+++ lisp/languages/ops-lang.el
@@ -30,7 +30,9 @@
 (use-package dockerfile-mode
   :commands dockerfile-mode
   :init
-  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
+  (add-to-list
+   'auto-mode-alist
+   '("Dockerfile\\'" . dockerfile-mode)))
 
 (use-package puppet-mode
   :commands puppet-mode)
