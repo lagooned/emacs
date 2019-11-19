ffa93810826138def3703e25492f589c482f294b
jared engler <jared.lite@gmail.com>
2019-11-19 00:23:43 -0600

    remove time from modeline

--- lisp/global.el
+++ lisp/global.el
@@ -187,10 +187,6 @@
 (setq ring-bell-function 'ignore)
 (setq visible-bell nil)
 
-;; time in modeline
-(setq display-time-default-load-average 'nil)
-(display-time-mode 1)
-
 (provide 'global)
 ;;; global.el ends here
 
