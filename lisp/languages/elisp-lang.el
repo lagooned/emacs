cc85cd58ba71643ca81b1aa5d3f9da710a638240
Jared Engler <jared.lite@gmail.com>
2019-12-29 01:44:49 -0600

    not sure why it has to be this way but ok

--- lisp/languages/elisp-lang.el
+++ lisp/languages/elisp-lang.el
@@ -31,12 +31,15 @@
   (evil-leader/set-key-for-mode 'lisp-interaction-mode "e" 'eval-last-sexp)
   :hook ((emacs-lisp-mode . aggressive-indent-mode)
          (emacs-lisp-mode . prettify-symbols-mode)
-         (emacs-lisp-mode . je/configure-elisp-company-backends)
-         (lisp-interaction-mode . prettify-symbols-mode)
-         (lisp-interaction-mode . je/configure-elisp-company-backends))
+         (emacs-lisp-mode . je/configure-elisp-company-backends))
   :config
   (which-key-add-major-mode-key-based-replacements 'emacs-lisp-mode "SPC e" "eval-sexp")
   (which-key-add-major-mode-key-based-replacements 'lisp-interaction-mode "SPC e" "eval-sexp"))
+
+(use-package lisp-interaction-mode
+  :hook
+  ((lisp-interaction-mode . prettify-symbols-mode)
+   (lisp-interaction-mode . je/configure-elisp-company-backends)))
 
 (use-package eldoc
   :diminish eldoc-mode
