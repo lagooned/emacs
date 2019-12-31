8a9a3b0dfe82c2f5d4d80104418c80c97b4e1305
Jared Engler <jared.lite@gmail.com>
2019-12-31 00:59:15 -0600

    remove old tiny

--- elpa/tiny-20170903.949/tiny-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tiny" "20170903.949" "Quickly generate linear ranges in Emacs" 'nil :commit "012b2e7a67b9f067bbfa0292479861ffbaa201fa" :keywords '("convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/tiny")
