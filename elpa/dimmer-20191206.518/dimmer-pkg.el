18987fc3a55ecb66c6034c6769435493d60d9a7b
Jared Engler <jared.lite@gmail.com>
2019-12-31 01:20:18 -0600

    delete old dimmer

--- elpa/dimmer-20191206.518/dimmer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dimmer" "20191206.518" "visually highlight the selected buffer" '((emacs "25")) :commit "ddbd6216fff0d2eea14141d735f661be1c16815e" :keywords '("faces" "editing") :authors '(("Neil Okamoto")) :maintainer '("Neil Okamoto") :url "https://github.com/gonewest818/dimmer.el")
