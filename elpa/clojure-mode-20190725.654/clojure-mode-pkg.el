be060d0b296f3bb32c6db25382a4a6ee85a80c12
Jared Engler <jared.lite@gmail.com>
2019-12-31 01:19:45 -0600

    delete old clojure-mode

--- elpa/clojure-mode-20190725.654/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/jared/.emacs.d/elpa/clojure-mode-20190725.654/clojure-mode.el  -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20190725.654" "Major mode for Clojure code" '((emacs "25.1")) :commit "f23eb209a8bedec95e0ad0542762bd13998ba048" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
