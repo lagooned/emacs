643a647b0690228bdea9f1ff02044fd439d00008
Jared Engler <jared.lite@gmail.com>
2019-12-31 01:15:16 -0600

    remove old git-commit

--- elpa/git-commit-20190102.107/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190102.107" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "80f2f7cbf02fbc0e2c7a1ae086729bcf9789d1b7" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
