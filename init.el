228bcb2fa3f7a8b9abc52962b47dce5535c69b6e
Jared Engler <jared.lite@gmail.com>
2020-06-06 01:47:27 -0500

    activate melpa-stable
    also disable melpa bleeding edge

--- init.el
+++ init.el
@@ -35,8 +35,9 @@
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("marmalade" . "https://marmalade-repo.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
         ;; ("melpa-stable-mirror" . "https://www.mirrorservice.org/sites/stable.melpa.org/packages/")
+        ;; ("melpa" . "https://melpa.org/packages/")
         ;; ("melpa-mirror" . "https://www.mirrorservice.org/sites/melpa.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 
