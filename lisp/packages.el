907c4f32c1bf2bdaff77b2cac1481b349031b265
Jared Engler <jared.lite@gmail.com>
2020-06-07 09:01:15 -0500

    remove term ansi-term multi-term from evil collection mode

--- lisp/packages.el
+++ lisp/packages.el
@@ -197,6 +197,7 @@
 (use-package evil-collection
   :after evil
   :config
+  (setq evil-collection-mode-list (remove `(term term ansi-term multi-term) evil-collection-mode-list))
   (evil-collection-init)
   (evil-collection-define-key 'normal 'dired-mode-map
     "." 'dired-omit-mode
