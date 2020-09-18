77bcf174d9ff157cec458edfb787a96e9dd3aa47
Jared Engler <jared.lite@gmail.com>
2020-09-18 04:52:30 -0500

    upgrade packages 09182020

---
+++ elpa/modus-vivendi-theme-20200918.401/modus-vivendi-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-vivendi-theme" "20200918.401" "Accessible dark theme (WCAG AAA)"
+  '((emacs "26.1"))
+  :commit "aeaf598ae8cd84100243e0fe084545ea18397b90" :keywords
+  '("faces" "theme" "accessibility")
+  :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Protesilaos Stavrou" . "info@protesilaos.com")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
