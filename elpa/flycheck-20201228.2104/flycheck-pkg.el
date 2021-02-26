557805667249971dff10e58841ea19eff222fc0f
Jared Engler <jared.lite@gmail.com>
2021-02-25 19:31:35 -0600

    update flycheck

--- elpa/flycheck-20201228.2104/flycheck-pkg.el
+++ elpa/flycheck-20201228.2104/flycheck-pkg.el
@@ -1,15 +1,15 @@
-(define-package "flycheck" "20200820.1403" "On-the-fly syntax checking"
+(define-package "flycheck" "20201228.2104" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
     (seq "1.11")
     (emacs "24.3"))
-  :commit "806f2a8c21231a0dddd0a48d27fa4021729ee365" :keywords
-  '("convenience" "languages" "tools")
-  :authors
+  :commit "01396a5eff9fa494285e0d3139838231c05e3948" :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainer
   '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
+  :keywords
+  '("convenience" "languages" "tools")
   :url "http://www.flycheck.org")
 ;; Local Variables:
 ;; no-byte-compile: t
